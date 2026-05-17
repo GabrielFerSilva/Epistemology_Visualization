47a950f870ecc59db1ed5ac0a2ee7deecdc0298d
Bruno <al.bruno.paula@impatech.edu.br>
2026-05-17 00:15:13 -0300

    structure introduced

---
+++ script.js
@@ -0,0 +1,13 @@
+const epistemologos = [{
+    id: "000001",
+    nome: "Francis Bacon",
+    data_nascimento : "1561-01-22",
+    data_morte: "1626-04-09",
+    livros: ["Novum Organum"]
+},{
+    id: "000002",
+    nome: "Karl Popper",
+    data_nascimento: "1902-07-28",
+    data_morte: "1994-09-17",
+    livros: ["The logic of Scientific Discovery"]
+}];
