127c43ce1e969b19d8b379f6918a8899f61fabe9
arthur zaneti <afzaneti1@gmail.com>
2026-05-17 21:16:14 -0300

    Adicionei um resumo com as ideias principais de Bacon e Popper. Coloquei o que considero como as ideias principais deles no const ideias mas não sei se essa formulação das ideias principais deles em afirmações curtas sera o melhor para a visualização.
    O item de ID 15 sobre o problema de Duhen-Quine
    tem fonte meio vaga mas deixei como as principais
    obras dos autores (pelo que entendi). Ele quebra
    um pouco a organização bonitinha dos outros items
    que tem autor Popper ou Bacon mas acho que precisa
    ser incluído por ser a melhor crítica a Popper que
    eu encontrei.

--- script.js
+++ script.js
@@ -26,6 +26,87 @@ const ideas = [{
     setence: "Não pode haver um conjunto de regras adequadas de escolha que se possam impor ao desejado comportamento individual nos casos concretos que os cientistas encontrarão no decorrer de suas carreiras",
     book: "Criticism and the Growth of Knowledge",
     writer: "Thomas Kuhn"
+},{
+    id: "000000002",
+    setence: "O conhecimento não pode ser gerado de modo puramente intelectual, ele deve ser baseado na observação, experimento e indução",
+    book: "Novum Organum",
+    writer: "Francis Bacon"},
+{
+    id: "000000003",
+    setence: "Métodos dedutivos so conseguem revelar conclusões que estavam escondidas nas premissas, não podem gerar conhecimentos novos por si próprios.",
+    book: "Novum Organum",
+    writer: "Francis Bacon"},
+{
+    id: "000000004",
+    setence: "Indução: Podemos inferir leis gerais da natureza a partir da observação controlada, repetida e sistemática de fenômenos particulares",
+    book: "Novum Organum",
+    writer: "Francis Bacon"},
+{
+     id: "000000005",
+    setence: "Os \"Ídolos\" de Bacon são tendências naturais do pensamento humano que distorcem nossa compreensão da realidade e dificultam a investigação científica objetiva.",
+    book: "Novum Organum",
+    writer: "Francis Bacon"
+},
+{
+    id: "000000006",
+    setence: "Ídolos da Tribo: Erros da natureza humana, enxergar padrões e confirmações onde elas não existem por exemplo.",
+    book: "Novum Organum",
+    writer: "Francis Bacon"
+},
+{
+    id: "000000007",
+    setence: "Ídolos da Caverna: Erros causados pelos vieses pessoais do observador, da sua \"caverna\" mental",
+    book: "Novum Organum",
+    writer: "Francis Bacon"
+},
+{
+    id: "000000008",
+    setence: "Ídolos do Mercado: Erros gerados pela linguagem imprecisa e pelo uso confuso das palavras.",
+    book: "Novum Organum",
+    writer: "Francis Bacon"
+},
+{
+    id: "000000009",
+    setence: "Ídolos do Teatro: Aceitação de sistemas intelectuais sem verificação. Confundir o teatro pela realidade. ",
+    book: "Novum Organum",
+    writer: "Francis Bacon"
+},
+{
+    id: "0000000010",
+    setence: "Popper respeitava muito a Teoria da Relatividade de Einstein pois ela faz previsões arriscadas que podem ser falseadas",
+    book: "The Logic of Scientific Discovery",
+    writer: "Karl Popper"
+},
+{
+    id: "0000000011",
+    setence: "A ciência avança por conjecturas ousadas e refutações, não pela verificação.",
+    book: "The Logic of Scientific Discovery",
+    writer: "Karl Popper"
+},
+{
+    id: "0000000012",
+    setence: "Uma teoria é científica se, e somente se, é falseável.",
+    book: "The Logic of Scientific Discovery",
+    writer: "Karl Popper"
+},
+{
+    id: "0000000013",
+    setence: "Nenhuma teoria científica é provada definitivamente verdadeira. Se a teoria sobreviveu a repetidos e variados testes de falsificação ela ganha reputação, mas nunca pode ser provada definitivamente verdadeira.",
+    book: "The Logic of Scientific Discovery",
+    writer: "Karl Popper"
+},
+{
+    id: "0000000014",
+    setence: "Observação e experimentos são importantes como métodos pelos quais se tenta falsear uma teoria",
+    book: "The Logic of Scientific Discovery",
+    writer: "Karl Popper"
+},
+,
+{
+    id: "0000000015",
+    setence: "Problema de Duhem-Quine: Verificar falseabilidade de um teoria de modo não ambíguo é impossível. Uma teoria nunca é testada isoladamente. Podemos sempre atribuir a falha de predição da teoria a um erro do experimento em si ou das hipóteses",
+    book: "The Aim and Structure of Physical Theory / Two Dogmas of Empiricism",
+    writer: "Pierre Duhem e Willard Van Orman Quine"
 }];
 
 function renderizarCirculos() {
